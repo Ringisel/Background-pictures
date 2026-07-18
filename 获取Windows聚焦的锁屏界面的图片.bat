ff75a14a0b7f0bf9aacd52aacce5a2d2863c645d
Ringisel <W3.141592653589793238462643383279502@outlook.com>
2026-07-18 14:04:55 +0800

    Add files via upload

---
+++ 获取Windows聚焦的锁屏界面的图片.bat
@@ -0,0 +1,5 @@
+start %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+@echo off
+echo .
+echo 我们已为你打开该文件夹，请将里面的无后缀文件复制，再在每个文件后面加上后缀“.jpg”或“.png”
+pause
